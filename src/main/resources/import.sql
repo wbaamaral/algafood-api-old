9e1555d244e725824709c503f7b4a6b7110dd848
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-27 14:47:31 -0400

    03.07 - importando dados de teste com import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha (nome) values ('Tailandesa');
+insert into cozinha (nome) values ('Indiana');
+insert into cozinha (nome) values ('Portuguesa');
+insert into cozinha (nome) values ('Brasileira');

3dbb892af959f511c06d5e493c6f3bc89d78a35f
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-27 10:10:46 -0400

    02.17 - desanbiguacao de beans com primary

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.di.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +10,12 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 public class AtivacaoClienteService {
 
 	@Autowired
-	private List<Notificador> notificadores;
+	private Notificador notificador;
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		for (Notificador notificador : notificadores) {
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		}
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 
 }

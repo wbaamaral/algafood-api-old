3d28ee88e97e0fb6b06ae29ea80591ffe4ff08fc
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-27 10:14:21 -0400

    02.21 - criando metodos de callback do ciclo de vida de outros beans

--- src/main/java/com/algaworks/algafood/ServiceConfig.java
+++ src/main/java/com/algaworks/algafood/ServiceConfig.java
@@ -3,15 +3,14 @@ package com.algaworks.algafood;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.service.AtivacaoClienteService;
 
 @Configuration
 public class ServiceConfig {
 
-	@Bean
-	public AtivacaoClienteService ativacaoClienteService(Notificador notificador) {
-		return new AtivacaoClienteService(notificador);
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	public AtivacaoClienteService ativacaoClienteService() {
+		return new AtivacaoClienteService();
 	}
 	
 }

e6a0f8baf41afa5a3d68965c956c73e7f1e92dc2
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-27 14:39:22 -0400

    02.27 - alterando a configuração do projeto dependendo do ambiente com spring profiles

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorProperties.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorProperties.java
@@ -7,27 +7,27 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("notificador.email")
 public class NotificadorProperties {
 
-	/**
-	 * Host do servidor de e-mail
-	 */
-	private String hostServidor;
-	
-	/**
-	 * Porta do servidor de e-mail
-	 */
-	private Integer portaServidor = 25;
-	
-	public String getHostServidor() {
-		return hostServidor;
-	}
-	public void setHostServidor(String hostServidor) {
-		this.hostServidor = hostServidor;
-	}
-	public Integer getPortaServidor() {
-		return portaServidor;
-	}
-	public void setPortaServidor(Integer portaServidor) {
-		this.portaServidor = portaServidor;
-	}
-	
+  /**
+   * Host do servidor de e-mail
+   */
+  private String hostServidor;
+  
+  /**
+   * Porta do servidor de e-mail
+   */
+  private Integer portaServidor = 25;
+  
+  public String getHostServidor() {
+    return hostServidor;
+  }
+  public void setHostServidor(String hostServidor) {
+    this.hostServidor = hostServidor;
+  }
+  public Integer getPortaServidor() {
+    return portaServidor;
+  }
+  public void setPortaServidor(Integer portaServidor) {
+    this.portaServidor = portaServidor;
+  }
+  
 }

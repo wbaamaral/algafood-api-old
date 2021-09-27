9b18426e54625cdd7c4ef55f50db9a9878f75ae4
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-27 11:33:44 -0400

    ajuste repositorio

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,13 +3,19 @@ package com.algaworks.algafood.di.service;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import com.algaworks.algafood.di.notificacao.Notificador;
 
 @Component
 public class AtivacaoClienteService {
 
-	private NotificadorEmail notificador;
+	private Notificador notificador;
 	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		
+		System.out.println("AtivacaoClienteService: " + notificador);
+	}
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 

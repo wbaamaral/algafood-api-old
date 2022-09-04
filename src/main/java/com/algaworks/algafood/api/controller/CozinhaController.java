c455c0d6a6eed9afb5fefeec8ba91556ba6f45aa
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-03 20:04:39 -0400

    4.25. Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.di.modelo.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import net.bytebuddy.asm.Advice.Return;
 
 @RestController
 @RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -55,5 +59,21 @@ public class CozinhaController {
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 
 		return cozinhaRepository.salvar(cozinha);
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+			cozinhaRepository.salvar(cozinhaAtual);
+
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }

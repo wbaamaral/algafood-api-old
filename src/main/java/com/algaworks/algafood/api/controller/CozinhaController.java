4b644af98465e954f03523a498ac45bd17b8a417
Welyqrson Bastos Amaral <welyqson@gmail.com>
2022-09-12 17:44:35 -0400

    4.33. Analisando solução para atualização parcial de recursos com PATCH

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -101,5 +103,12 @@ public class CozinhaController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+	}
+
+	@PatchMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizarParcial(@PathVariable Long cozinhaId,
+			@RequestBody Map<String, Object> campoCozinha) {
+
+		return ResponseEntity.ok(cadastroCozinha.merge(cozinhaId, campoCozinha));
 	}
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -1,14 +1,20 @@
 package com.algaworks.algafood.domain.service;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.ReflectUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class CadastroCozinhaService {
@@ -44,6 +50,49 @@ public class CadastroCozinhaService {
 			throw new EntidadeEmUsoException(
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso.", cozinhaId));
 		}
+	}
+
+	public Cozinha merge(Long cozinhaId, Map<String, Object> dadosOrigem) {
+
+		/*
+		 * preencher o objeto com dados do banco... criar um ojectmapper para mapear o
+		 * que foi passado pela api criar um field para receber o valor com base na
+		 * classe origem
+		 * 
+		 * usar o getfield para atribuir valor ao field com base no objeto convertido
+		 * pelo serializador do jackson
+		 * 
+		 * por fim atualizar o objeto cozinha atualizar com base no resultdo da
+		 * conversão
+		 * 
+		 */
+		Cozinha cozinhaAtualizar = buscarCozinha(cozinhaId);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Cozinha cozinhaOrigem = objectMapper.convertValue(dadosOrigem, Cozinha.class);
+
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Cozinha.class, nomePropriedade);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, cozinhaOrigem);
+
+			ReflectionUtils.setField(field, cozinhaAtualizar, novoValor);
+
+		});
+
+		return salvar(cozinhaAtualizar);
+
+	}
+
+	public Cozinha buscarCozinha(Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		if (cozinha == null)
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não exite um cadastro de cozinha com o código %d", cozinhaId));
+		else
+			return cozinha;
 	}
 
 }

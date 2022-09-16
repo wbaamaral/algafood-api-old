6161f3644bebe359ddd6cc97c53095242c8e4fb8
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-15 23:49:55 -0400

    5.4. Refatorando o código do projeto para usar o repositório do SDJ - cidade

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.domain.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;

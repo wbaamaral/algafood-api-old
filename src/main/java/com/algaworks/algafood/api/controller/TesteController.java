42e7986cc47e54da1b68477f13eff693f7fd0239
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-18 12:44:21 -0400

    classe teste

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,26 +1,39 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/teste")
+@RequestMapping("/testes")
 public class TesteController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
-	@GetMapping("/cozinhas/por-nome")
-	public List<Cozinha> cozinhasPorNome(@RequestParam("cozinha") String nome) {
+	@GetMapping("/cozinhas/list-por-nome")
+	public List<Cozinha> listarCozinhasPorNome(String nome) {
 
-		return cozinhaRepository.listarPorNome(nome);
+		return cozinhaRepository.findBynome(nome);
+
+	}
+
+	@GetMapping("/cozinhas/por-nome")
+	public ResponseEntity<Cozinha> cozinhasPorNome(String nome) {
+		Optional<Cozinha> cozinha = cozinhaRepository.nome(nome);
+
+		if (cozinha.isPresent()) {
+			return ResponseEntity.ok(cozinha.get());
+		} else {
+			return ResponseEntity.noContent().build();
+		}
 	}
 }
--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,18 +11,18 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 	/*
-	 * Para fins didáticos...
-	 * é possível criar consultas personalizadas utilizando a immplementação
-	 * dinâmica do jpa, seguindo os seguintes critérios...
+	 * Para fins didáticos... é possível criar consultas personalizadas utilizando a
+	 * immplementação dinâmica do jpa, seguindo os seguintes critérios...
 	 * 
 	 */
-	
-	//Retorna uma lista de cozinhas buscando por "nome", utilizando o nome do atributo para uma 
-	//busca exata e retornando uma lista de cozinhas
-	List<Cozinha> nome(String nome);
-	
+
+	// Retorna uma lista de cozinhas buscando por "nome", utilizando o nome do
+	// atributo para uma
+	// busca exata e retornando uma lista de cozinhas
+	Optional<Cozinha> nome(String nome);
+
 	/*
 	 * Busca similar ao criterio like, prefixado "fidby" atributo
 	 */
-	List<Cozinha> findbyNome(String nome);
+	List<Cozinha> findBynome(String nome);
 }

4e56d0d35115394864460345d384638ae82ab315
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-18 09:59:10 -0400

    5.6. Criando consultas com query methods - clase de testes e consultas personalizadas

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,19 @@ import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-
+	/*
+	 * Para fins didáticos...
+	 * é possível criar consultas personalizadas utilizando a immplementação
+	 * dinâmica do jpa, seguindo os seguintes critérios...
+	 * 
+	 */
+	
+	//Retorna uma lista de cozinhas buscando por "nome", utilizando o nome do atributo para uma 
+	//busca exata e retornando uma lista de cozinhas
+	List<Cozinha> nome(String nome);
+	
+	/*
+	 * Busca similar ao criterio like, prefixado "fidby" atributo
+	 */
+	List<Cozinha> findbyNome(String nome);
 }

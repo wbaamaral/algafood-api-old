82ee794efb6b799cd145cd5199988886b604cd37
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-20 20:24:16 -0400

    5.12. Implementando uma consulta dinâmica com JPQL

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
@@ -17,13 +20,32 @@ public class RestauranteRepositoryImpl {
 	private EntityManager manager;
 
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+		var parametros = new HashMap<String, Object>();
 
-		var jpql = "from Restaurante where nome like :nome "
-				+ " and taxaFrete between :taxaFreteInicial and :taxaFreteFinal";
+		var jpql = new StringBuilder();
 
-		return manager.createQuery(jpql, Restaurante.class).setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaFreteInicial", taxaFreteInicial)
-				.setParameter("taxaFreteFinal", taxaFreteFinal)
-				.getResultList();
+		jpql.append("from Restaurante where 0 = 0 ");
+
+		if (StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%" + nome + "%");
+		}
+
+		if (taxaFreteInicial != null) {
+			jpql.append("and taxaFrete >= :taxaFreteInicial ");
+			parametros.put("taxaFreteInicial", taxaFreteInicial);
+		}
+
+		if (taxaFreteFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFreteFinal ");
+			parametros.put("taxaFreteFinal", taxaFreteFinal);
+		}
+
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+
+		return query.getResultList();
+
 	}
 }

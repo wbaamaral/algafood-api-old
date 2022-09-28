82a19565f2e0e9b0f7dbae55427ded587344d106
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-09-28 04:31:34 -0400

    Removendo lixo

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -17,18 +17,10 @@ public interface RestauranteRepository
 
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-<<<<<<< HEAD
-	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
-	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 
-	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
-=======
 	// @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
-
-	// List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long
-	// cozinha);
->>>>>>> main
 
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 
